cde897712e1a92c6018a33a88deee960e77ef6db
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-25 18:31:28 +0100

    refactor HSV color space converter
    Format code, use PartialConversionResult class

--- src/models/converters/HSVColorSpaceConverter.ts
+++ src/models/converters/HSVColorSpaceConverter.ts
@@ -4,9 +4,7 @@ import { PartialConversionResult } from 'models/conversion-results/PartialConver
 import { ColorSpaceConverter } from 'models/converters/ColorSpaceConverter';
 
 export class HSVColorSpaceConverter implements ColorSpaceConverter {
-  public convertFromImageData(
-    imageData: ImageData
-  ): ColorSpaceConversionResult {
+  public convertFromImageData(imageData: ImageData): ColorSpaceConversionResult {
     const { data, width, height } = imageData;
     const lastPixelIndex = width * height * 4 - 4;
     const hValues: number[] = [];
@@ -14,58 +12,40 @@ export class HSVColorSpaceConverter implements ColorSpaceConverter {
     const vValues: number[] = [];
 
     for (let index = 0; index <= lastPixelIndex; index += 4) {
-      const { h, s, v } = this.convertSinglePixel(
-        data[index],
-        data[index + 1],
-        data[index + 2]
-      );
+      const { h, s, v } = this.convertSinglePixel(data[index], data[index + 1], data[index + 2]);
       hValues.push(h);
       sValues.push(s);
       vValues.push(v);
     }
 
-    return this.createConversionResult(
-      hValues,
-      sValues,
-      vValues,
-      width,
-      height
-    );
+    return this.createConversionResult(hValues, sValues, vValues, width, height);
   }
 
-  public convertFromRGB(
-    r: number,
-    b: number,
-    g: number
-  ): ColorSpaceConversionResult {
+  public convertFromRGB(r: number, g: number, b: number): ColorSpaceConversionResult {
     const { h, s, v } = this.convertSinglePixel(r, g, b);
 
     return this.createConversionResult([h], [s], [v], 1, 1);
   }
 
-  private convertSinglePixel(
-    r: number,
-    g: number,
-    b: number
-  ): { h: number; s: number; v: number } {
+  private convertSinglePixel(r: number, g: number, b: number): { h: number; s: number; v: number } {
     const result = {
       h: 0,
       s: 0,
       v: Math.max(r, g, b)
     };
 
-    const x = Math.min(r, g, b); // temp
+    const min = Math.min(r, g, b);
 
-    if (x === result.v) {
+    if (min === result.v) {
       result.h = 0;
       result.s = 0;
     } else {
       if (result.v === r) {
-        result.h = 0 + (g - b) * 60 / (result.v - x);
-      } else if (x === g) {
-        result.h = 120 + (b - r) * 60 / (result.v - x);
-      } else if (x === b) {
-        result.h = 240 + (r - g) * 60 / (result.v - x);
+        result.h = 0 + (g - b) * 60 / (result.v - min);
+      } else if (min === g) {
+        result.h = 120 + (b - r) * 60 / (result.v - min);
+      } else if (min === b) {
+        result.h = 240 + (r - g) * 60 / (result.v - min);
       }
     }
 
@@ -76,7 +56,7 @@ export class HSVColorSpaceConverter implements ColorSpaceConverter {
     if (result.v === 0) {
       result.s = 0;
     } else {
-      result.s = (result.v - x) * 100 / result.v;
+      result.s = (result.v - min) * 100 / result.v;
     }
 
     result.v *= 100 / 255;
@@ -91,31 +71,12 @@ export class HSVColorSpaceConverter implements ColorSpaceConverter {
     width: number,
     height: number
   ): HSVColorSpaceConversionResult {
-    const hComponent: PartialConversionResult = {
-      values: hValues,
-      label: 'H',
-      width,
-      height
-    };
+    const hComponent = new PartialConversionResult('H', hValues, width, height);
 
-    const sComponent: PartialConversionResult = {
-      values: sValues,
-      label: 'S',
-      width,
-      height
-    };
+    const sComponent = new PartialConversionResult('S', sValues, width, height);
 
-    const vComponent: PartialConversionResult = {
-      values: vValues,
-      label: 'V',
-      width,
-      height
-    };
+    const vComponent = new PartialConversionResult('V', vValues, width, height);
 
-    return new HSVColorSpaceConversionResult(
-      hComponent,
-      sComponent,
-      vComponent
-    );
+    return new HSVColorSpaceConversionResult(hComponent, sComponent, vComponent);
   }
 }
