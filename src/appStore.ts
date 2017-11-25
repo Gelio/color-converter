ef606f144dfa692f191c4ed6b4080f8f55425e5e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-25 22:11:00 +0100

    add output reducer to application store

--- src/appStore.ts
+++ src/appStore.ts
@@ -1,13 +1,16 @@
 import { combineReducers, createStore } from 'redux';
 
 import { inputReducer, InputState } from 'reducers/inputReducer';
+import { outputReducer, OutputState } from 'reducers/outputReducer';
 
 export interface AppState {
   input: InputState;
+  output: OutputState;
 }
 
 const reducer = combineReducers<AppState>({
-  input: inputReducer
+  input: inputReducer,
+  output: outputReducer
 });
 
 export const appStore = createStore<AppState>(
