4adec803cc090660c1df40002e4fbe5e2c65bbe2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-26 12:20:30 +0100

    add conditional parameter box rendering

--- src/containers/ConversionParameterBox.ts
+++ src/containers/ConversionParameterBox.ts
@@ -1,29 +1,45 @@
 import { HyperContainer } from 'utils/HyperContainer';
 
 import { AppState } from 'appStore';
+import { LabParameterBox } from 'components/LabParameterBox';
+import { ColorProfileType } from 'models/ColorProfileType';
 import { ColorSpaceType } from 'models/ColorSpaceType';
 import { ConversionParameters } from 'models/ConversionParameters';
+import { IlluminantType } from 'models/IlluminantType';
 
 interface ContainerState {
   selectedColorSpace: ColorSpaceType;
   conversionParameters: ConversionParameters;
+  illuminantType: IlluminantType;
+  colorProfileType: ColorProfileType;
 }
 
 export class ConversionParameterBox extends HyperContainer<ContainerState> {
   protected mapAppStateToContainerState(appState: AppState): ContainerState {
     return {
       selectedColorSpace: appState.input.selectedColorSpace,
-      conversionParameters: appState.input.conversionParameters
+      conversionParameters: appState.input.conversionParameters,
+      illuminantType: appState.input.illuminantType,
+      colorProfileType: appState.input.colorProfileType
     };
   }
 
   protected render() {
     return this.html`
       <div onconnected=${this} ondisconnected=${this}>
-        Conversion parameter box
-
-        ${this.state.selectedColorSpace}
+        ${this.getColorSpaceParameterBox()}
       </div>
     `;
+  }
+
+  private getColorSpaceParameterBox() {
+    const { illuminantType, colorProfileType, conversionParameters } = this.state;
+
+    return {
+      [ColorSpaceType.HSV]: () => null,
+      [ColorSpaceType.YCbCr]: () => null,
+      [ColorSpaceType.Lab]: () =>
+        LabParameterBox(illuminantType, colorProfileType, conversionParameters)
+    }[this.state.selectedColorSpace]();
   }
 }
